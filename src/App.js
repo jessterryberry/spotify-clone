15de29cb69181a4225abb1d93b3ec6b3fcf9bb9e
Jessica Terryberry <83835789+jessterryberry@users.noreply.github.com>
2022-10-21 10:10:56 -0400

    component updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
 import "./App.css";
 import Login from "./Login";
-import { getTokenFromUrl } from "./spotify";
+
+const s = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState();
+  const [{ token }, dispatch] = useStateValue();
 
   useEffect(() => {
-    const hash = getTokenFromUrl();
+    // Set token
+    const hash = getTokenFromResponse();
     window.location.hash = "";
-    const _token = hash.access_token;
+    let _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      s.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
     }
+  }, [token, dispatch]);
 
-    console.log("token", token);
-  }, []);
-
-  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
+  return (
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={s} />}
+    </div>
+  );
 }
 
 export default App;
--- src/spotify.js
+++ src/spotify.js
@@ -1,26 +1,27 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "https://localhost:3000/callback";
+const redirectUri = "http://localhost:3000/";
 const clientId = "c356d4549f14402aa015e15abdf52ff2";
 
 const scopes = [
-    "user-read-currently-playing",
-    "user-read-recently-played",
-    "user-read-playback-state",
-    "user-top-read",
-    "user-modify-playback-state",
-  ];
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
 
-export const getTokenFromUrl = () => {
+export const getTokenFromResponse = () => {
   return window.location.hash
     .substring(1)
     .split("&")
     .reduce((initial, item) => {
-      let parts = item.split("=");
+      var parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
+
       return initial;
     }, {});
 };
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-    "%20"
-  )}&response_type=token&show_dialog=true`;
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
